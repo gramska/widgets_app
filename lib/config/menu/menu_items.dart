9f6b6380c6fede21b48e8658bb4c6d5c4c519e28
Gabo Ramos <gabinoramos@gmail.com>
2025-09-29 19:33:47 -0600

    home_screen creado

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+
+  final String title;
+  final String subtitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title, 
+    required this.subtitle, 
+    required this.link, 
+    required this.icon
+  });
+}
+
+const appMenuItems = <MenuItem>[
+    
+    MenuItem(
+      title: 'Targetas', 
+      subtitle: 'Un contenedor estilizado', 
+      link: '/card', 
+      icon: Icons.credit_card
+    ),
+  ];
